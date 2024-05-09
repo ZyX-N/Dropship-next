f566662391ee52a03e2547bdfb3fd8f06ef5f2ca
Aman <aman.softfix@gmail.com>
2024-05-09 19:16:30 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,56 +2,141 @@ import { BannerSlider } from "@/components/carousel/bannerSlider";
 import Image from "next/image";
 
 export default function Home() {
-
-  const banners:any = [
+  const banners: any = [
     {
-      title:"Slide 1",
-      image:"https://flowbite.com/docs/images/carousel/carousel-1.svg"
+      title: "Slide 1",
+      image: "https://gamlewala.in:5555/public/17138792890300_Banner2.webp",
     },
     {
-      title:"Slide 2",
-      image:"https://flowbite.com/docs/images/carousel/carousel-2.svg"
+      title: "Slide 2",
+      image: "https://gamlewala.in:5555/public/17138793140030_Banner3.webp",
     },
     {
-      title:"Slide 3",
-      image:"https://flowbite.com/docs/images/carousel/carousel-3.svg"
-    }
-  ]
+      title: "Slide 3",
+      image: "https://gamlewala.in:5555/public/17138794405480_Banner5.webp",
+    },
+  ];
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-10">
-      <div className="w-full">
-        <BannerSlider data={banners}/>
-      </div>
-      <section className="text-gray-600 body-font">
-        <div className="container mx-auto flex px-5 py-20 items-center justify-center flex-col">
-          <img
-            className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
-            alt="hero"
-            src="https://dummyimage.com/720x600"
-          />
-          <div className="text-center lg:w-2/3 w-full">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              Microdosing synth tattooed vexillologist
-            </h1>
-            <p className="mb-8 leading-relaxed">
-              Meggings kinfolk echo park stumptown DIY, kale chips beard
-              jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice
-              godard disrupt ramps hexagon mustache umami snackwave tilde
-              chillwave ugh. Pour-over meditation PBR&B pickled ennui celiac
-              mlkshk freegan photo booth af fingerstache pitchfork.
-            </p>
-            <div className="flex justify-center">
-              <button className="inline-flex text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">
-                Button
-              </button>
-              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-                Button
-              </button>
-            </div>
+    <>
+      <nav className="bg-white border-gray-200">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a
+            href="https://flowbite.com/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-8"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap">
+              Flowbite
+            </span>
+          </a>
+          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            <button
+              type="button"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
+            >
+              Get started
+            </button>
+            <button
+              data-collapse-toggle="navbar-cta"
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+              aria-controls="navbar-cta"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+          </div>
+          <div
+            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            id="navbar-cta"
+          >
+            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white ">
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700"
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700"
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700"
+                >
+                  Services
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
-      </section>
-    </main>
+      </nav>
+      <div className="w-full">
+        <BannerSlider data={banners} />
+      </div>
+      <main className="flex min-h-screen flex-col px-10">
+        <section className="text-gray-600">
+          <div className="container mx-auto flex py-16 items-center justify-center flex-col">
+            <div className="text-center lg:w-2/3 w-full">
+              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+                Microdosing synth tattooed vexillologist
+              </h1>
+              <p className="mb-8 leading-relaxed">
+                Meggings kinfolk echo park stumptown DIY, kale chips beard
+                jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice
+                godard disrupt ramps hexagon mustache umami snackwave tilde
+                chillwave ugh. Pour-over meditation PBR&B pickled ennui celiac
+                mlkshk freegan photo booth af fingerstache pitchfork.
+              </p>
+              <div className="flex justify-center">
+                <button className="inline-flex text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">
+                  Button
+                </button>
+                <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                  Button
+                </button>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
--- src/components/carousel/bannerSlider.tsx
+++ src/components/carousel/bannerSlider.tsx
@@ -1,35 +1,55 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function BannerSlider({ data }: { data: Array<any> }) {
-  const [slide, setSlide] = useState<number>(1);
+  const [slide, setSlide] = useState<number>(0);
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+      setSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1));
+    }, 4000);
+
+    return () => clearInterval(timer);
+  }, []);
 
   return (
     <div className="w-full">
       <div id="default-carousel" className="relative">
-        <div className="overflow-hid den flex relative w-full h-56 rounded-lg sm:h-64 xl:h-80 2xl:h-96">
-          {data?.map((item: any) => (
+        <div className="overflow-hidden flex w-full h-56 rounded-lg sm:h-64 xl:h-80 2xl:h-96">
+          {data?.map((item: any, index: number) => (
             <div
-              className={`absolute duration-700 ease-in-out w-full h-full`}
+              className={`absolute duration-700 ease-in-out bg-green-300 rounded-lg left-0 top-0 w-full h-full ${
+                slide === index ? "opacity-100 z-20" : "opacity-0 z-10"
+              }`}
               key={item.title}
             >
               <img
                 src={item.image}
-                className="w-96 h-full"
+                className="w-full h-full rounded-lg"
                 alt={item.title}
               />
             </div>
           ))}
         </div>
+
         <div className="flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2">
-          <button type="button" className="w-3 h-3 rounded-full"></button>
-          <button type="button" className="w-3 h-3 rounded-full"></button>
-          <button type="button" className="w-3 h-3 rounded-full"></button>
+          {data?.map((item: any, index: number) => (
+            <button
+              key={index}
+              type="button"
+              className={`w-3 h-3 rounded-full ${
+                index === slide ? "bg-gray-300" : "bg-gray-100"
+              }`}
+              onClick={() => setSlide(index)}
+            ></button>
+          ))}
         </div>
+
         <button
           type="button"
           className="flex absolute top-0 left-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none"
+          onClick={() => setSlide(slide > 0 ? slide - 1 : data.length - 1)}
         >
           <span className="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
             <svg
@@ -49,9 +69,11 @@ export function BannerSlider({ data }: { data: Array<any> }) {
             <span className="hidden">Previous</span>
           </span>
         </button>
+
         <button
           type="button"
           className="flex absolute top-0 right-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none"
+          onClick={() => setSlide(slide < data.length - 1 ? slide + 1 : 0)}
         >
           <span className="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
             <svg
